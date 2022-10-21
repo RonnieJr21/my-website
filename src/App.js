9096875da6badf65ed3cb16dfe9493e16a8a5bd9
Rjhj21 <92937892+RonnieJr21@users.noreply.github.com>
2022-10-20 20:44:33 -0400

    first update

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ function App() {
     <div className="App">
      <h1>My react app</h1>
       <h2>Author: Ronnie Huggins</h2>
+        <h3> Hi there</h3>
+        <p> lorem Ipsum</p>
     </div>
   );
 }
